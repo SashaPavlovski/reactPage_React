e4411b987521539422cd2fc73b4358e9a83bd237
SashaPavlovski <sasha123.ez@gmailcom>
2022-11-22 23:04:38 +0200

    home work

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import "./app.css";
+import { BodyComponent } from "./components/bodyComponent/bodyComponent";
+import { HeaderComponet } from "./components/index";
+
+export const App = () => {
+  return (
+    <>
+      <HeaderComponet />
+      <BodyComponent />
+    </>
+  );
+};
